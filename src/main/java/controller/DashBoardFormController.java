c71b6725f24d02b6aea456e4e84d262eabf099fb
Sahan Samudra <sahansamudra123@gmail.com>
2023-04-03 11:02:32 +0530

    Second Commit

---
+++ src/main/java/controller/DashBoardFormController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class DashBoardFormController {
+
+
+    public AnchorPane DashboardFormContext;
+    public AnchorPane DisplayDashboard;
+    public AnchorPane AllContext;
+
+    public void btnLogout(ActionEvent actionEvent) throws IOException {
+
+        Stage window = (Stage) DashboardFormContext.getScene().getWindow();
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+        window.centerOnScreen();
+    }
+
+    public void btnCustomer(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../view/CustomerForm.fxml");
+        assert resource != null;
+        Parent load = FXMLLoader.load(resource);
+        AllContext.getChildren().clear();
+        AllContext.getChildren().add(load);
+    }
+
+    public void btnHome(ActionEvent actionEvent) throws IOException {
+        home();
+    }
+
+    public void initialize() throws IOException {
+        home();
+    }
+
+
+    public void home() throws IOException {
+        URL resource = getClass().getResource("../view/DisplayHome.fxml");
+        assert resource != null;
+        Parent load = FXMLLoader.load(resource);
+        AllContext.getChildren().clear();
+        AllContext.getChildren().add(load);
+
+    }
+
+    public void btnPayment(ActionEvent actionEvent) {
+    }
+
+    public void btnSupplier(ActionEvent actionEvent) {
+    }
+
+    public void btnEmployee(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginFormController {
+    public Label lblError;
+    public JFXPasswordField Password;
+
+    public AnchorPane LoginFormContext;
+
+    public JFXTextField UserName;
+
+    public void btnLogin(ActionEvent actionEvent) throws IOException {
+
+
+        LoginFormManager();
+
+    }
+    public void LoginFormManager() throws IOException {
+        String user = "U";
+        String password = "1";
+        if (UserName.getText().equals(user) && Password.getText().equals(password)) {
+            Stage window = (Stage) LoginFormContext.getScene().getWindow();
+            window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashBoardForm.fxml"))));
+            window.centerOnScreen();
+        } else if (UserName.getText().isEmpty() && Password.getText().isEmpty()) {
+            lblError.setText("Your User Name Or Password IS Empty...!");
+            UserName.clear();
+            Password.clear();
+        }
+        else if (!UserName.getText().equals(user)) {
+            lblError.setText("Your User Name is incorrect..!");
+            UserName.clear();
+            Password.clear();
+        } else if (!Password.getText().equals(password)) {
+            lblError.setText("Your Password is incorrect..!");
+            UserName.clear();
+            Password.clear();
+        }
+    }
+}
