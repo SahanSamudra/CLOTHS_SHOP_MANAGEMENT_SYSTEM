08b9a31cd2ee8cf66885d209a7ff6017cb8e6a8a
Sahan Samudra <sahansamudra123@gmail.com>
2023-04-20 01:51:19 +0530

    Update ItemForm.fxml

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -3,16 +3,47 @@ package controller;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import model.ItemModel;
+import model.Supplier;
+import tm.Item;
+import tm.ItemTm;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class ItemFormController {
     public AnchorPane ItemContext;
-    public TableView tblItems;
+
+    @FXML
+    private TableView<ItemTm> tblItems;
+
+
+    @FXML
+    void cmbSupplier0(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbTypeO(ActionEvent event) {
+
+    }
+
     public TableColumn colItemId;
     public TableColumn colItemType;
     public TableColumn colSize;
@@ -24,8 +55,8 @@ public class ItemFormController {
     public JFXTextField txtPrice;
     public JFXTextField txtQty;
     public JFXTextField txtItemId;
-    public JFXComboBox cmbType;
-    public JFXComboBox cmbSupplier;
+    public JFXComboBox<String> cmbType;
+    public JFXComboBox<String> cmbSupplier;
     public JFXDatePicker DatePicker;
     public Button btnSave;
 
@@ -37,14 +68,139 @@ public class ItemFormController {
     }
 
     public void btnSearchItem(ActionEvent actionEvent) {
+        String id = txtItemId.getText();
+        try {
+            Item item = ItemModel.search(id);
+            if (item != null) {
+                txtItemId.setText(item.getId());
+                cmbType.getSelectionModel().getSelectedItem();
+                txtPrice.setText(String.valueOf(item.getPrice()));
+                txtQty.setText(String.valueOf(item.getQty()));
+                txtSize.getText();
+                cmbSupplier.getSelectionModel().getSelectedItem();
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Customer Not Found").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void btnItemSave(ActionEvent actionEvent) {
+        String id = txtItemId.getText();
+        String type = String.valueOf(cmbType.getSelectionModel().getSelectedItem());
+        Double price = Double.valueOf(txtPrice.getText());
+        int qty = Integer.parseInt(txtQty.getText());
+        String size = txtSize.getText();
+        String supliyer = String.valueOf(cmbSupplier.getSelectionModel().getSelectedItem());
+
+        Item item = new Item(id, type, price, qty, size, supliyer);
+
+        try {
+            boolean isSave = ItemModel.save(item);
+            if (isSave) {
+                loadTable();
+                new Alert(Alert.AlertType.CONFIRMATION, "OK").show();
+
+            }
+        } catch (SQLException e) {
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void btnItemUpdate(ActionEvent actionEvent) {
+    public void btnItemUpdate(ActionEvent actionEvent) throws ClassNotFoundException {
+        String id = txtItemId.getText();
+        String type = String.valueOf(cmbType.getSelectionModel().getSelectedItem());
+        Double price = Double.valueOf(txtPrice.getText());
+        int qty = Integer.parseInt(txtQty.getText());
+        String size = txtSize.getText();
+        String supliyer = String.valueOf(cmbSupplier.getSelectionModel().getSelectedItem());
+
+        Item item = new Item(id, type, price, qty, size, supliyer);
+        try {
+            boolean isUpdate = ItemModel.update(item);
+            if (isUpdate) {
+                new Alert(Alert.AlertType.CONFIRMATION, "OK").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void btnItemDelete(ActionEvent actionEvent) {
+        String id = txtItemId.getText();
+        try {
+            boolean isDelete = ItemModel.delete(id);
+            if (isDelete) {
+                new Alert(Alert.AlertType.CONFIRMATION, "OK").show();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    void loadTable() {
+        try {
+            ObservableList<ItemTm> obList = FXCollections.observableArrayList();
+            ResultSet all = ItemModel.getAll();
+            while (all.next()){
+                String stringPrice = all.getString(3);
+                String qty = all.getString(4);
+                obList.add(new ItemTm(all.getString(1),all.getString(2),Double.parseDouble(stringPrice),Integer.parseInt(qty)
+                        ,all.getString(5),all.getString(6)));
+            }
+            tblItems.setItems(obList);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void initialize() {
+        setCellValueFactory();
+        loadSupplierId();
+        setCmbTypeComboxData();
+        loadTable();
+    }
+
+    private void setCmbTypeComboxData() {
+        ObservableList<String> type=FXCollections.observableArrayList();
+        type.add("Shirt");
+        type.add("Tshirt");
+        cmbType.setItems(type);
+    }
+
+    private void loadSupplierId() {
+        ObservableList<String> observableList = FXCollections.observableArrayList();
+        try {
+            List<String> supplierId = Supplier.searchSupplierId();
+            for (String code : supplierId) {
+                observableList.add(code);
+            }
+            cmbSupplier.setItems(observableList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    void setCellValueFactory() {
+        colItemId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colItemType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        colSize.setCellValueFactory(new PropertyValueFactory<>("size"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colSupplier.setCellValueFactory(new PropertyValueFactory<>("supID"));
     }
 }
--- src/main/java/controller/PaymentFormController.java
+++ src/main/java/controller/PaymentFormController.java
@@ -1,4 +1,156 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Duration;
+import model.ItemModel;
+import tm.Item;
+import tm.ItemTm;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class PaymentFormController {
+
+    public TableView tblItems;
+    public TableColumn colItemId;
+    public TableColumn colItemType;
+    public TableColumn colSize;
+    public TableColumn colPrice;
+    public TableColumn colQty;
+    public TableColumn colDate;
+    public Label lblOrderId;
+    public JFXTextField txtPrice;
+    public JFXTextField txtSize;
+    public JFXTextField txtItemType;
+    public JFXTextField txtQty;
+    public JFXTextField txtQtyOnHand;
+    public Label lblDate1;
+    public Label lblItemCount;
+    public Label lblDiscount;
+    public Label lblFinalTotal;
+    public JFXButton btnPlaceOrder;
+    public JFXComboBox cmbItemID;
+    public JFXButton btnClear;
+    public JFXButton btnAddToCart;
+    ObservableList<String> itemCode= FXCollections.observableArrayList();
+    LinkedHashMap<JFXTextField, Pattern> map = new LinkedHashMap<>();
+    Pattern qtyPattern = Pattern.compile("^[0-9]{1,6}$");
+
+    private void storeValidations(){
+        map.put(txtQty, qtyPattern);
+
+    }
+
+    int cartSelectedRawRemove = -1;
+
+
+    public void initialize(){
+        btnAddToCart.setDisable(true);
+        storeValidations();
+        initClock();
+        setOrderId();
+        itemCodeSetComboBoxData();
+
+
+        tblItems.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
+
+            cartSelectedRawRemove= (int) newValue;
+        });
+
+    }
+
+    private void itemCodeSetComboBoxData() {
+        try {
+            ResultSet allItemsCode = ItemModel.getAllItemsCode();
+
+            while (allItemsCode.next()){
+
+                itemCode.add(allItemsCode.getString(1));
+
+            }
+            cmbItemID.setItems(itemCode);
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    public Label lblDate;
+    public Label lblTime;
+
+    private void initClock() {
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            lblTime.setText(LocalDateTime.now().format(formatter));
+
+
+            SimpleDateFormat formatter2 = new SimpleDateFormat("dd-MM-yyyy");
+            Date date = new Date();
+            lblDate.setText(formatter2.format(date));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    private void setOrderId(){
+
+        try {
+            lblOrderId.setText(new  OrderFormController().getOrderId());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void btnPlaceOrder(ActionEvent actionEvent) {
+    }
+
+    public void btnClear(ActionEvent actionEvent) {
+    }
+
+    public void btnAddToCart(ActionEvent actionEvent) {
+    }
+
+
+    public void itemSelectOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        int selectedIndex = cmbItemID.getSelectionModel().getSelectedIndex();
+        String s = itemCode.get(selectedIndex);
+        Item item = ItemModel.search(s);
+        int qty = item.getQty();
+        String size = item.getSize();
+        String supplier = item.getSupplier();
+        String type = item.getType();
+        double price = item.getPrice();
+        txtQtyOnHand.setText(qty+"");
+        txtSize.setText(size);
+        txtPrice.setText(price+"");
+    }
 }
